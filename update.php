149c92ebfe52ce7a67648999e0c2916508e50bee
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2021-10-29 19:02:41 +0000

    Header e Footer

--- update.php
+++ update.php
@@ -4,11 +4,13 @@ require_once './vendor/autoload.php';
 use ExemploPDOMySQL\MySQLConnection;
 
 $bd = new MySQLConnection();
+
 $genero = null;
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $comando = $bd->prepare('SELECT * FROM generos WHERE id = :id');
     $comando->execute([':id' => $_GET['id']]);
+
     $genero = $comando->fetch(PDO::FETCH_ASSOC);
 } else {
     $comando = $bd->prepare('UPDATE generos SET nome = :nome WHERE id = :id');
@@ -20,17 +22,12 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     header('Location:/index.php');
 }
 
+$_title = 'Editar Gênero';
+
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <title>Editar Gênero</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    </head>
-    <body>
-        <main class="container">
+<?php include('./includes/header.php'); ?>
+
             <h1>Editar Gênero</h1>
             <form action="update.php" method="post">
                 <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
@@ -42,6 +39,5 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
                 <a class="btn btn-secondary" href="index.php">Voltar</a>
                 <button class="btn btn-success" type="submit">Salvar</button>
             </form>
-        </main>
-    </body>
-</html>
+        
+<?php include('./includes/footer.php');
